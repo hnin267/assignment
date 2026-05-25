3b4760b37701c4ec69e8f9269853c5d5ea6807b4
hnin267 <hayeshwe@gmail.com>
2026-05-25 20:51:33 +0630

    update of javascript

--- grocery.js
+++ grocery.js
@@ -1,27 +1,34 @@
-let product = [
+let products = [
   { name: "Bread", price: 2.5 },
   { name: "Milk", price: 3.0 },
   { name: "Eggs", price: 2.0 },
-  { name: "Chesse", price: 4.5 },
+  { name: "Cheese", price: 4.5 },
   { name: "Apples", price: 5.0 },
   { name: "Chicken", price: 8.0 },
   { name: "Rice", price: 3.5 },
   { name: "Butter", price: 2.75 },
   { name: "Juice", price: 3.25 },
 ];
-let total_Cost = 0;
-for (let i = 0; i < product.length; i++) {
-  total_Price += product[i].price;
+
+let total_Price = 0;
+
+for (let i = 0; i < products.length; i++) {
+  total_Price += products[i].price;
 }
-console.log(`The Total Cost : $ ${total_Cost}`);
+
+console.log(`The Total Cost: $${total_Price.toFixed(2)}`);
+
 let discount = 0.1;
 let cost = total_Price * discount;
+
 total_Price = total_Price - cost;
-console.log(`The discount applied : $ ${cost}`);
-// tax
+
+console.log(`The discount applied: $${cost.toFixed(2)}`);
+
 let tax = 0.07;
 let taxpayment = total_Price * tax;
+
 total_Price = total_Price + taxpayment;
-console.log(`Tax amount : $ ${taxpayment}`);
-// final amount
-console.log(`The final amount : $ ${total_Price}`);
+
+console.log(`Tax amount: $${taxpayment.toFixed(2)}`);
+console.log(`The final amount: $${total_Price.toFixed(2)}`);
